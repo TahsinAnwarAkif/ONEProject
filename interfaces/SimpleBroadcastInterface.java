daf5f4ef8064314d10ec7d194c0000abccf7dafb
AKIF <AKIF@DESKTOP-F95RQ1U>
2017-01-11 01:31:49 +0600

    5th Commit,Upto NodeInfoUpdate Working

--- interfaces/SimpleBroadcastInterface.java
+++ interfaces/SimpleBroadcastInterface.java
@@ -9,9 +9,7 @@ import java.util.Collection;
 import core.CBRConnection;
 import core.Connection;
 import core.DTNHost;
-import static core.DTNHost.MaliciousNodes;
-import static core.DTNHost.MsgInfo;
-import static core.DTNHost.NodeInfo;
+
 import static core.DTNHost.sortMsgInfo;
 import core.NetworkInterface;
 import core.Settings;
@@ -50,150 +48,13 @@ public class SimpleBroadcastInterface extends NetworkInterface {
         DTNHost[] node = new DTNHost[1000];
         for(int i = 0; i <n1.NodeInfo.size(); i++)
             {
-                ArrayList tmp =(ArrayList)NodeInfo.get(i);
+                ArrayList tmp =(ArrayList)n1.NodeInfo.get(i);
                 node[i]       = (DTNHost)tmp.get(0);
                 if(node[i] == n2)return true;
             }
         
         return false;
         }
-        
-        //UPDATING A DTNHOST
-        public static void NodeInfoUpdate(DTNHost n)
-        {
-         DTNHost[] src = new DTNHost[10000]; 
-         DTNHost[] dest= new DTNHost[10000];
-         int flag1 = 0,flag2 = 0;
-         
-         DTNHost[] node = new DTNHost[1000]; 
-         int[]     FTT  = new int[1000];
-         int[]     RTT  = new int[1000];
-         double[]  Ratio= new double[1000];
-         
-         for(int i = 0; i <n.MsgInfo.size(); i++)
-            {
-                ArrayList tmp =(ArrayList)n.MsgInfo.get(i);
-                src[i]        = (DTNHost)tmp.get(1);
-                dest[i]       = (DTNHost)tmp.get(2);
-            }
-         for(int i = 0; i <n.NodeInfo.size(); i++)
-            {
-                ArrayList  tmp     =(ArrayList)n.NodeInfo.get(i);
-                           
-                           node[i] =(DTNHost)tmp.get(0); 
-                           FTT[i]  =(int) tmp.get(1);
-                           RTT[i]  =(int) tmp.get(2);
-                           Ratio[i]=(int) tmp.get(3);
-                
-                if(src[i] == node[i])
-                    {
-                        
-                        Ratio[i] = (double)((FTT[i]+1)/RTT[i]);
-                        
-                        tmp.set(1,FTT[i]+1);
-                        tmp.set(3,Ratio[i]);    
-                    }
-                else 
-                    {
-                        ArrayList tmp2 = new ArrayList();
-                        
-                        tmp2.add(src[i]);
-                        tmp2.add(1);
-                        tmp2.add(0);
-                        tmp2.add(POSITIVE_INFINITY);
-                        
-                        n.NodeInfo.add(tmp2);
-                
-                    }
-                
-                if(dest[i] == node[i])
-                    {
-                        Ratio[i] = (double)(FTT[i]/(RTT[i]+1));
-                        
-                        tmp.set(2,RTT[i]+1);
-                        tmp.set(3,Ratio);    
-                    }
-                else 
-                    {
-                        ArrayList tmp2 = new ArrayList();
-                        
-                        tmp2.add(dest[i]);
-                        tmp2.add(0);
-                        tmp2.add(1);
-                        tmp2.add(0);
-                        
-                        n.NodeInfo.add(tmp2);
-                
-                    }
-            }
-          
-        }
-        
-        public static void getAllMsgs(DTNHost from,DTNHost to)
-        {
-         ArrayList fromMsg = from.MsgInfo;
-         ArrayList toMsg   = to.MsgInfo;
-         
-         
-         
-         if(fromMsg.size() == 0 && toMsg.size() == 0) return;
-         
-         
-         else
-             {
-               ArrayList Ti1 = (ArrayList) fromMsg.get(0);
-               ArrayList Ti2 = (ArrayList) toMsg.get(0);
-         
-               String T1 = (String) Ti1.get(0);
-               String T2 = (String) Ti2.get(0);  
-               
-               //update n1 node
-               if (T2.compareTo(T1) > 0)
-               {
-                String[] Time  = new String[1000];   
-                DTNHost[] src  = new DTNHost[1000];
-                DTNHost[] dest = new DTNHost[1000];
-                
-                for(int i = 0; i < toMsg.size() ; i++)
-                    {     
-                         ArrayList tmp = (ArrayList)toMsg.get(i);
-                         Time[i] = (String)tmp.get(0);
-                         if(Time[i].compareTo(T1) > 0) fromMsg.add(tmp);
-                         else break;
-                    }
-                
-               from.MsgInfo = fromMsg;
-               sortMsgInfo(from); 
-               
-               }
-               
-               //update n2 node
-               else if (T2.compareTo(T1) < 0)
-               {
-                String[] Time  = new String[1000];   
-                DTNHost[] src  = new DTNHost[1000];
-                DTNHost[] dest = new DTNHost[1000];
-                
-                for(int i = 0; i < fromMsg.size() ; i++)
-                    {     
-                         ArrayList tmp = (ArrayList)fromMsg.get(i);
-                         Time[i] = (String)tmp.get(0);
-                         if(Time[i].compareTo(T2) > 0) toMsg.add(tmp);
-                         else break;
-                    }
-               to.MsgInfo = toMsg; 
-               sortMsgInfo(to); 
-               
-               }
-             
-             
-             
-             
-             }
-            
-            
-             }
-        
         /**
 	 * Tries to connect this host to another host. The other host must be
 	 * active and within range of this host for the connection to succeed. 
@@ -221,8 +82,8 @@ public class SimpleBroadcastInterface extends NetworkInterface {
 			Connection con = new CBRConnection(this.host, this, 
 					anotherInterface.getHost(), anotherInterface, conSpeed);
                         
-                        getAllMsgs(this.host,anotherInterface.getHost());
-                        NodeInfoUpdate(this.host);
+                        //getAllMsgs(this.host,anotherInterface.getHost());
+                        //NodeInfoUpdate(this.host);
                        
                         /*
                         int n;
@@ -255,7 +116,7 @@ public class SimpleBroadcastInterface extends NetworkInterface {
 			assert con.isUp() : "Connection " + con + " was down!";
                         
                         int f = 0;
-                        for(int j = 0; j < MaliciousNodes.length; j++)
+                        for(int j = 0; j < this.host.MaliciousNodes.length; j++)
                                 {
                                     if (anotherInterface.getHost().getAddress() == this.host.MaliciousNodes[j])
                                         f = 1;
